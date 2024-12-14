16df8295c539da0f61f24d3b7c8b234ff761abf0
zuzinkohtun-chl <zuzinkohtun@cityholdings.com.mm>
2024-12-14 19:51:38 +0630

    Initial Setup

---
+++ ConcurrentAPIsCallDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var httpClient = new HttpClient();
+var url = "https://smsgw.preprod.citymall.com.mm/api/client/getclients"; // Replace with your API endpoint
+int concurrentUsers = 200;
+
+// Create a list of tasks representing concurrent users
+var tasks = Enumerable.Range(1, concurrentUsers)
+					  .Select(async user =>
+					  {
+						  var response = await httpClient.GetAsync(url);
+						  var content = await response.Content.ReadAsStringAsync();
+
+						  Console.WriteLine($"User {user} received: {response.StatusCode}, Content: {content}");
+					  });
+
+// Execute all tasks concurrently
+await Task.WhenAll(tasks);
+
+Console.WriteLine("All requests completed.");
---
+++ ConcurrentAPIsCallDemo/RequestModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcurrentAPIsCallDemo
+{
+	public class RequestModel
+	{
+		public string? From { get; set; }
+		public required string To { get; set; }
+		public required string Text { get; set; }
+	}
+}
